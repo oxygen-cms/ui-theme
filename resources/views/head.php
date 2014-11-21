3475fd8ae1c72082fa48ed10f33e8902eb132685
Chris Chamberlain <c.chamberlain@iinet.net.au>
2014-11-22 07:33:40 +1100

    Fixed floating main nav and added `theme-color` meta tag

--- resources/views/head.php
+++ resources/views/head.php
@@ -1,3 +1,5 @@
+<meta name="theme-color" content="#333">
+
 <link rel="stylesheet" href="/packages/oxygen/ui/css/main.css">
 <link href="http://fonts.googleapis.com/css?family=Roboto:400,400italic,700|Roboto+Slab" rel="stylesheet" type="text/css">
 <script type="text/javascript" src="/packages/oxygen/ui/js/vendor/modernizr-2.8.3.min.js"></script>
